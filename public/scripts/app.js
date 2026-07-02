8f0f4961658a21528f2920ed27c7785ca4629845
yosuf-dev <yosuf.saleh.zadeh@gmail.com>
2026-07-02 14:51:59 +0330

    Coffee Shop

---
+++ public/scripts/app.js
@@ -0,0 +1,58 @@
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+const submenuOpenBtn = document.querySelector(".submenu-open-btn");
+const submenu = document.querySelector(".submenu");
+const navOpenBtn = document.querySelector(".nav-icon");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+const nav = document.querySelector(".nav");
+const cartOpenBtn = document.querySelector(".cart-icon");
+const cartCloseBtn = document.querySelector(".cart-close-btn");
+const cart = document.querySelector(".cart");
+const overlay = document.querySelector(".overlay");
+
+toggleThemeBtns.forEach(btn => {
+    btn.addEventListener("click", function (){
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }
+    })
+})
+submenuOpenBtn.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.classList.toggle("text-orange-300");
+    submenu.classList.toggle("submenu--open");
+    submenuOpenBtn.classList.toggle("rotate-180");
+})
+
+
+navOpenBtn.addEventListener("click",() => {
+    nav.classList.remove("-right-64");
+    nav.classList.add("right-0");
+    overlay.classList.add("overlay--visible");
+    overlay.addEventListener("click",closeNav);
+})
+
+cartOpenBtn.addEventListener("click",() => {
+    cart.classList.remove("-left-64");
+    cart.classList.add("left-0");
+    overlay.classList.add("overlay--visible");
+    overlay.addEventListener("click",closeCart);
+})
+
+function closeNav() {
+    nav.classList.remove("right-0");
+    nav.classList.add("-right-64");
+    overlay.classList.remove("overlay--visible");
+}
+
+function closeCart() {
+    cart.classList.remove("left-0");
+    cart.classList.add("-left-64");
+    overlay.classList.remove("overlay--visible");
+}
+
+navCloseBtn.addEventListener("click",closeNav);
+cartCloseBtn.addEventListener("click",closeCart);
+
